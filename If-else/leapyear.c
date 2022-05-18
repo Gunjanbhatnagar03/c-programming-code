95e706bf6e88a658135fd58f83ec550bf05923ef
Gunjan Bhatnagar <104645080+Gunjanbhatnagar03@users.noreply.github.com>
2022-05-19 00:04:08 +0530

    Add files via upload

---
+++ If-else/leapyear.c
@@ -0,0 +1,19 @@
+// to check whether a year is leap year not
+
+#include<stdio.h>
+#include<conio.h>
+ int main(){
+ 	int year;
+ 	printf("Enter the year");
+ 	scanf("%d",&year);
+ 	 if(year%4==0){
+	  
+ 	 printf("This year is a leap year");
+ }
+ else
+ {
+ 	 	 printf("This year is not a leap year");
+
+ }
+ return 0;
+}
