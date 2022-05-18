e71207696113e698dd35a882edbd1eff37af99b6
Gunjan Bhatnagar <104645080+Gunjanbhatnagar03@users.noreply.github.com>
2022-05-18 23:47:10 +0530

    Add files via upload

---
+++ If-else/integers.c
@@ -0,0 +1,21 @@
+
+//whether to check a number is positive or negative
+
+#include<stdio.h>
+#include<conio.h>
+ int main(){
+ 	int num1;
+ 	printf("enter the value of num1");
+ 	scanf("%d",&num1);
+ 	 if(num1>0)
+ 	 {
+	  printf("It is a positive number");
+}
+else 
+ 	
+{
+	printf("It is a negative number");
+	 } 	
+ return 0;
+ }
+ 
