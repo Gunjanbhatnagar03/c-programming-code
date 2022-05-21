bfd8afdd9e98d86ee63881e417faa2d0c56debfb
Gunjan Bhatnagar <104645080+Gunjanbhatnagar03@users.noreply.github.com>
2022-05-21 16:01:23 +0530

    Add files via upload

---
+++ loops/10_natural numbers.cpp
@@ -0,0 +1,14 @@
+//display the first  10 natural numbers
+
+#include <stdio.h>
+
+int main()
+{
+	printf("The First 10 Natural Numbers are\n");
+	
+	for (int i = 1; i <= 10; i++)
+	{
+		printf("%d\n", i);
+	}
+}
+
