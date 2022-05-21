c5a743ce795541dbd6f0893bcb1a0e2ba86b9379
Gunjan Bhatnagar <104645080+Gunjanbhatnagar03@users.noreply.github.com>
2022-05-21 14:27:58 +0530

    Add files via upload

---
+++ loops/Table.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+int i, num, table;
+	printf("\nEnter num");
+	scanf("%d",&num);
+	
+
+ for(i=1;i<=10;i ++)
+ { 
+ table=  num*i;
+ 
+printf("\n%d*%d=%d",num,i,table);
+}
+return 0;
+
+}
+
+
+
+
